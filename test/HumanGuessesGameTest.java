3c824e55311ab9f738322c8ae798bdb471f9fd93
evhly <HutchinsEJ19@gcc.edu>
2025-03-13 17:05:43 -0400

    added a couple more unit tests for HumanGuessesGame

--- test/HumanGuessesGameTest.java
+++ test/HumanGuessesGameTest.java
@@ -12,10 +12,39 @@ class HumanGuessesGameTest {
     }
 
     @Test
-    void getNumGuesses() {
+    void makeHighGuess() {
+        HumanGuessesGame g = new HumanGuessesGame(10);
+        GuessResult result = g.makeGuess(50);
+        assertEquals(GuessResult.HIGH, result);
     }
 
     @Test
-    void isDone() {
+    void makeCorrectGuess() {
+        HumanGuessesGame g = new HumanGuessesGame(10);
+        GuessResult result = g.makeGuess(10);
+        assertEquals(GuessResult.CORRECT, result);
     }
+
+    @Test
+    void makeVeryHighGuess() {
+        HumanGuessesGame g = new HumanGuessesGame(10);
+        GuessResult result = g.makeGuess(80000000);
+        assertEquals(GuessResult.HIGH, result);
+    }
+
+    @Test
+    void makeVeryLowGuess() {
+        HumanGuessesGame g = new HumanGuessesGame(10);
+        GuessResult result = g.makeGuess(-60);
+        assertEquals(GuessResult.LOW, result);
+    }
+
+//    @Test
+//    void getNumGuesses() {
+//    }
+//
+//    @Test
+//    void isDone() {
+//    }
+    //i dont think these need to be tested
 }
